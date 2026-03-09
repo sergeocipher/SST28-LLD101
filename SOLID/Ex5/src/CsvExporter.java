d1b17e2fcf1047bd9a66f9db784838e1b71ba47a
Somyajit <somyajitsaha621@gmail.com>
2026-03-09 12:06:22 +0530

    changes

--- SOLID/Ex5/src/CsvExporter.java
+++ SOLID/Ex5/src/CsvExporter.java
@@ -4,7 +4,7 @@ public class CsvExporter extends Exporter {
     @Override
     public ExportResult doExport(ExportRequest req) {
         // LSP issue: changes meaning by lossy conversion
-        String body = req.body == null ? "" : req.body.replace("\n", " ").replace(",", " ");
+        String body = req.body == null ? "" : req.body.replace("\n", "\\n");
         String csv = "title,body\n" + req.title + "," + body + "\n";
         return new ExportResult("text/csv", csv.getBytes(StandardCharsets.UTF_8));
     }
